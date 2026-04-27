2c4f90c1716cb7ed573977ae5e66f14555426456
tr16an <triggerrred@gmail.com>
2026-04-27 17:50:15 +0300

    feature3

--- src/ex03/day01_ex03.sql
+++ src/ex03/day01_ex03.sql
@@ -1,8 +1,6 @@
-SELECT p.name AS person_name,
-       m.pizza_name,
-       pz.name AS pizzeria_name
-FROM person_order AS po
-JOIN person AS p ON po.person_id = p.id
-JOIN menu AS m ON po.menu_id = m.id
-JOIN pizzeria AS pz ON m.pizzeria_id = pz.id
-ORDER BY person_name ASC, pizza_name ASC, pizzeria_name ASC;
+SELECT order_date AS action_date, person_id
+FROM person_order
+INTERSECT
+SELECT visit_date AS action_date, person_id
+FROM person_visits
+ORDER BY action_date ASC, person_id DESC;
